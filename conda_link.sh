62c7acb8d81da07fc0e184b0ede4b1ec0b334216
ejolly <eshin.jolly@gmail.com>
2021-11-17 11:05:39 -0500

    add updated conda link script

--- conda_link.sh
+++ conda_link.sh
@@ -3,7 +3,7 @@
 # Link a separate kernel to an existing jupyter instance
 # Assumes your base conda config exists within your $HOME/.zshrc file
 if [ $# -eq 0 ]; then
-    printf "Make jupyter aware of a python kernel in another environment.\n\nUSAGE:\nconda_link 'envname'\n'envname' should match what you pass into the 'conda activate' command\n\nAssumes that conda configuration was setup in $HOME/.zshrc\n"
+    printf "Make jupyter aware of a python kernel in another environment.\n\nUSAGE:\nconda_link 'envname'\n'envname' should match what you pass into the 'conda activate' command\n\nTo remove an installed kernel run:\njupyter kernelspec remove 'envname'.\n\nTo see a list of installed kernels run:\njupyter kernelspec list\n\nAssumes that conda configuration was setup in $HOME/.zshrc if you're using zsh as your shell or $HOME/.bashrc if you're using bash as your shell. This should be true if you installed conda via the Anaconda commandline installer.\n"
 else
     source "$HOME/.zshrc"
     envexists=$(conda env list | grep "$1")
