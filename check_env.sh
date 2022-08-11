335f7aee5c8003a164df9653959559f0ed3b8e51
ejolly <eshin.jolly@gmail.com>
2022-08-11 15:32:20 -0400

    updated project setup script

--- check_env.sh
+++ check_env.sh
@@ -6,47 +6,43 @@
 # It will check for the existence of the following two files and diff them if they exist or create and git add them if they don't. From then on you can version control these files. Simply rerun this script anytime you conda install/uninstall/update anything to keep track of your environment changes.
 # You can recreate your current environment using: conda env create -f env.yml
 ENV_FILE='environment.yml'
-REV_FILE='environment-revisions.txt'
 
 # Ensure the user doesn't accidentally run this from a shell in which is the base environment is active
 CURRENT_ENV=$(echo "$CONDA_DEFAULT_ENV")
-if [ "$CURRENT_ENV" == "base" ]; then
-    echo "The base conda environment appears to be active. Use conda activate YOUR_ENV prior to running this script"
+if [ "$CURRENT_ENV" != "base" ]; then
+    echo "The conda env check pre-commit hook needs to be run from your base conda env. Deactivate any environments and try again"
     exit 1
 fi
 
 # If both the environment revisions file already exist then diff them
-if [ -f "$ENV_FILE" ] && [ -f "$REV_FILE" ]; then
-    echo "Checking conda environment for changes since last commit..."
+if [ -f "$ENV_FILE" ]; then
+    echo "Checking environment.yml against conda environment for changes since last commit..."
 
     # Get current environment revision list
-    CONDA_YAML=$(conda env export)
-    CONDA_REV=$(conda list --revisions)
+    CONDA_YAML=$(conda run -p ./env conda env export --no-builds)
 
     # Get diff with existing files
     DIFF=$(echo "$CONDA_YAML" | git diff --no-index -- "$ENV_FILE" -)
-    RDIFF=$(echo "$CONDA_REV" | git diff --no-index -- "$REV_FILE" -)
 
     if [ "$DIFF" != "" ]; then
-        echo "Changes found...updating $ENV_FILE"
+        echo "Changes found...updating and staging $ENV_FILE"
         echo "$CONDA_YAML" >"$ENV_FILE"
-        echo "$CONDA_REV" >"$REV_FILE"
+        git add "$ENV_FILE"
+        echo "Rerun your last git commit command to save changes"
+        exit 1
     else
-        echo "No changes found"
+        echo "No changes found...proceeding with commit"
+        exit 0
     fi
 else
     # Create the environment and revisions file for the first time
     if [ ! -f "$ENV_FILE" ]; then
         echo 'no env file found creating...'
-        conda env export -f "$ENV_FILE"
+        conda env export --no-builds -f "$ENV_FILE"
         git add "$ENV_FILE"
+        echo "Rerun your last git commit command to save changes"
+        exit 1
     fi
 
-    if [ ! -f "$REV_FILE" ]; then
-        echo 'no revisions file found creating...'
-        conda list --revisions >"$REV_FILE"
-        git add "$REV_FILE"
-    fi
 fi
 
-exit 0
--- init_project.py
+++ init_project.py
@@ -32,22 +32,41 @@ else:
 abs_project_path = os.path.join(base_dir, project_name)
 
 # Make dir structure
+print("Setting up project...")
 call_string = (
     "mkdir -p "
     + abs_project_path
-    + "/{analysis,data,papers,presentations,code,figures,paradigms};"
+    + "/{analysis,data,papers,presentations,code,figures,paradigms,.vscode};"
 )
 sys_call(call_string)
 
 # Create a README file
-call_string = "echo '# " + project_name + "' > " + abs_project_path + "/README.md"
-sys_call(call_string)
+readme = open(os.path.join(abs_project_path, "README.md"), "w")
+readme.write(
+    f"""
+# {project_name}
+
+
+---
+## Python environment setup
+The environment.yml file in this repo can be used to bootstrap a conda environment for
+reproducibility:\n
+`conda env create -p ./env -f environment.yml`\n
+
+To update the environment file after installing/removing packages: `conda env export --no-builds -f environment.yml`\n\n
+
+To update the environment itself after editing the `environment.yml` file: `conda env update --file environment.yml --prune`\n
+             """
+)
+readme.close()
 
 # Create a gitignore file
 ignore_file = open(os.path.join(abs_project_path, ".gitignore"), "w")
 ignore_file.write("*.DS_Store\n")
 ignore_file.write("*.ipynb_checkpoints\n")
 ignore_file.write("*.pyc\n")
+ignore_file.write("#Don't commit actual conda env\n")
+ignore_file.write("env\n")
 ignore_file.write("#Don't commit data, figs, or presentations by default\n")
 ignore_file.write("*.csv\n")
 ignore_file.write("*.txt\n")
@@ -58,13 +77,57 @@ ignore_file.write("*.ppt*\n")
 ignore_file.write("*.key\n")
 ignore_file.close()
 
-# Git
+# Vscode settings file
+vscode_file = open(os.path.join(abs_project_path, ".vscode", "settings.json"), "w")
+vscode_file.write("{\n")
+vscode_file.write(
+    '\t"python.defaultInterpreterPath": "${workspaceFolder}/env/bin/python",\n'
+)
+vscode_file.write('\t"python.terminal.activateEnvironment": true,\n')
+vscode_file.write('\t"editor.formatOnSave": true,\n')
+vscode_file.write('\t"python.analysis.extraPaths": ["${workspaceFolder}/code"]\n')
+vscode_file.write("}")
+vscode_file.close()
+
+# CD to project dir
 os.chdir(abs_project_path)
 # Create gitkeep files to commit empty dir structure
 call_string = "find * -type d -not -path '*/\.*' -exec touch {}/.gitkeep \;"
 sys_call(call_string)
-# Initialize, add all dirs and commit
-call_string = "git init && git add * && git add .gitignore"
-sys_call(call_string)
+# Initialize repo
+sys_call("git init")
+# Add stuff
+git_add_call_string = "git add .gitignore README.md .vscode analysis code data figures papers paradigms presentations"
+
+# Conda environment
+setup_py = input("Setup python environment? (y): ") or "y"
+if setup_py == "y":
+    py_version = input("Python version? (3.8): ") or "3.8"
+    setup_precommit = input("Setup environment check pre-commit hook? (y): ") or "y"
+    print("Setting up environment...")
+    call_string = f"conda create -y -p ./env python={py_version} pip ipykernel"
+    sys_call(call_string)
+    call_string = f"conda run -p ./env pip install --upgrade pycodestyle black flake8"
+    sys_call(call_string)
+    call_string = f"conda run -p ./env conda env export --no-builds -f environment.yml"
+    sys_call(call_string)
+
+    git_add_call_string += " environment.yml"
+
+# Perform initial commit
+sys_call(git_add_call_string)
 call_string = "git commit -m 'Initial project commit.'"
 sys_call(call_string)
+
+if setup_precommit:
+    print("Setting up pre-commit hook ...")
+    sys_call(
+        "cp /Users/Esh/Documents/cmd_programs/check_env.sh ./.git/hooks/pre-commit"
+    )
+
+# Messages
+print(f"New project folder and repo created in:\n\n {abs_project_path}")
+if setup_py:
+    print(
+        """\nNew python environment created in ./env with environment.yml packages\n\nYou can activate this environment in a terminal using: conda activate ./env\n"""
+    )
